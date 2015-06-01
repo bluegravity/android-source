e82fe3908818b9c04bc545b98bf561b9357709b9
Dan Lazarowski <dLazarowski@yahoo.com>
2015-06-01 13:53:48 -0400

    Lesson 14 done

--- foundation/14-best-practices/src/com/bloc/bestpractices/WorstPractices.java
+++ foundation/14-best-practices/src/com/bloc/bestpractices/WorstPractices.java
@@ -7,38 +7,54 @@ import java.io.InputStreamReader;
 public class WorstPractices extends Object {
 
 	/************************************************
-	 *	ASSIGNMENT:
-	 *	Fix code and comments below this block
-	/************************************************/
+	 * ASSIGNMENT: Fix code and comments below this block /
+	 ************************************************/
 
-	public static void main(String [] args) {
-	int MAGIC_NUMBER = WorstPractices.animals(false);
-	MAGIC_NUMBER *= 5;
-	if (MAGIC_NUMBER > 18) {
-	while(MAGIC_NUMBER > 0)
-	{
-	MAGIC_NUMBER--;
-	}
-	}
+	public static void main(String[] args) {
+		// get magic number
+		int magicNumber = WorstPractices.animals(false);
+		System.out.println(magicNumber);
+		magicNumber *= 5;
+		
+		// if magicNumber greater than 18, 
+		// reduce to zero in a really stupid way
+		if (magicNumber > 18) {
+			while (magicNumber > 0) {
+				magicNumber--;
+			}
+		}
+
+		System.out.println(magicNumber);
+
+		
 	}
 
-// animals
-// this method takes in a single parameter, yeswellheresthething. Using a very elaborate and complex algorithm, it calculate a magic number
-// yeswellheresthething: a seed which helps generate the magic number
-// returns: a magical number
-private static int animals(boolean yeswellheresthething)
-{
-		/*
-			Start off with one of these
-		*/
-	int aInt = yeswellheresthething ? 34 : 21;
-	float sparklesfairy = .5f;
-	for (int brown = 0; brown < aInt; brown++) { sparklesfairy *= aInt;
-	} return (int) sparklesfairy * aInt;
-}
+	// animals
+	// this method takes in a single parameter, input. Using a
+	// very elaborate and complex algorithm, it calculate a magic number
+	// input: a seed which helps generate the magic number
+	// returns: a magical number
+	private static int animals(boolean input) {
+
+		// set seed based on boolean input
+		int seedValue = input ? 34 : 21;
+		
+		System.out.println("seed " + seedValue);
+		float sparklesfairy = .5f;
+
+		// use a loop to multiply by (seed value^seed value)
+		for (int i = 0; i < seedValue; i++) {
+			sparklesfairy *= seedValue;
+			
+			System.out.println("SSS " + sparklesfairy);
+			
+		}
+		
+		// one more time on the way out!
+		return (int) sparklesfairy * seedValue;
+	}
 
 	/************************************************
-	 *	ASSIGNMENT
-	 *	Fix code and comments above this block
-	/************************************************/
+	 * ASSIGNMENT Fix code and comments above this block /
+	 ************************************************/
 }
