3636e51d2fc96e6a26ff9c751a4dfd166124c5f8
Dan Lazarowski <dLazarowski@yahoo.com>
2015-06-06 23:05:32 -0400

    lesson 27-collections

--- foundation/27-collections/src/com/bloc/collections/FavoritePastries.java
+++ foundation/27-collections/src/com/bloc/collections/FavoritePastries.java
@@ -20,100 +20,137 @@ import java.util.*;
 public class FavoritePastries {
 
 	/************************************************
- 	 *	ASSIGNMENT:
-	 *	Use a HashMap to store the relationship
-	 *	between rating and pastry: HashMap<Integer, List<Pastry>>
-	/************************************************/
+	 * ASSIGNMENT: Use a HashMap to store the relationship between rating and
+	 * pastry: HashMap<Integer, List<Pastry>> /
+	 ************************************************/
 
+	private HashMap<Pastry, Integer> ratingsHash = new HashMap<>();
 
 	public FavoritePastries() {
 		/************************************************
- 	 	 *	WORK HERE
-		/************************************************/
+		 * WORK HERE /
+		 ************************************************/
 	}
 
-	/* 
+	/*
 	 * addPastry
-	 *
+	 * 
 	 * Add a Pastry to the FavoritePastries class.
-	 *
-	 * This method stores this Pastry and its
-	 * associated rating in some sort of data structure.
-	 *
-	 * If this Pastry already exists in FavoritePastries,
-	 * its old rating should be updated.
-	 *
+	 * 
+	 * This method stores this Pastry and its associated rating in some sort of
+	 * data structure.
+	 * 
+	 * If this Pastry already exists in FavoritePastries, its old rating should
+	 * be updated.
+	 * 
 	 * @param pastry The Pastry to store
+	 * 
 	 * @param rating The rating to associate with it
+	 * 
 	 * @return nothing
 	 */
 	public void addPastry(Pastry pastry, int rating) {
 		/************************************************
- 	 	 *	WORK HERE
-		/************************************************/
+		 * WORK HERE /
+		 ************************************************/
+		// if exists, update rating
+		if (ratingsHash.containsKey(pastry)) {
+			// replace existing
+			ratingsHash.replace(pastry, rating);
+		} else {
+			// add new
+			ratingsHash.put(pastry, rating);
+		}
+
 	}
 
-	/* 
+	/*
 	 * removePastry
-	 *
+	 * 
 	 * Remove a Pastry from FavoritePastries
-	 *
-	 * This method removes any reference to this exact
-	 * Pastry object and its associated rating
-	 *
+	 * 
+	 * This method removes any reference to this exact Pastry object and its
+	 * associated rating
+	 * 
 	 * @param pastry The Pastry to remove
-	 * @return true if the Pastry was found and removed,
-	 *		   false otherwise
+	 * 
+	 * @return true if the Pastry was found and removed, false otherwise
 	 */
 	public boolean removePastry(Pastry pastry) {
 		/************************************************
- 	 	 *	WORK HERE, you must modify the return value
-		/************************************************/
-		return false;
+		 * WORK HERE, you must modify the return value /
+		 ************************************************/
+
+		boolean returnValue = false;
+
+		if (ratingsHash.containsKey(pastry)) {
+			// replace existing
+			ratingsHash.remove(pastry);
+			returnValue = true;
+		}
+
+		return returnValue;
 	}
 
-	/* 
+	/*
 	 * getRatingForPastry
-	 *
+	 * 
 	 * Return the associated rating for a given Pastry
-	 *
-	 * This method will find the associated rating for
-	 * this Pastry inside of FavoritePastries and return
-	 * it to its caller.
-	 *
-	 * @param  pastry The Pastry for which a rating must
-	 * 		   be recovered
-	 * @return the rating associated with this Pastry or
-	 *		   -1 if not found among FavoritePastries
+	 * 
+	 * This method will find the associated rating for this Pastry inside of
+	 * FavoritePastries and return it to its caller.
+	 * 
+	 * @param pastry The Pastry for which a rating must be recovered
+	 * 
+	 * @return the rating associated with this Pastry or -1 if not found among
+	 * FavoritePastries
 	 */
 	public int getRatingForPastry(Pastry pastry) {
 		/************************************************
- 	 	 *	WORK HERE, you must modify the return value
-		/************************************************/
-		return -1;
+		 * WORK HERE, you must modify the return value /
+		 ************************************************/
+
+		int returnValue = -1;
+
+		Integer i = ratingsHash.get(pastry);
+		if (i != null) {
+			returnValue = i;
+		}
+		return returnValue;
 	}
 
-	/* 
+	/*
 	 * getPastriesForRating
-	 *
-	 * Return a Set of all the Pastries with a given
-	 * rating.
-	 *
-	 * This method returns a Set<Pastry> object containing
-	 * references to all of the Pastries associated with
-	 * a particular rating.
-	 *
-	 * @param  rating The rating of the Pastry objects the
-	 *		   caller wishes to recover
-	 * @return A Set of all the Pastry objects with a rating
-	 * 		   of `rating`. Returns an empty set if none are
-	 *         found
+	 * 
+	 * Return a Set of all the Pastries with a given rating.
+	 * 
+	 * This method returns a Set<Pastry> object containing references to all of
+	 * the Pastries associated with a particular rating.
+	 * 
+	 * @param rating The rating of the Pastry objects the caller wishes to
+	 * recover
+	 * 
+	 * @return A Set of all the Pastry objects with a rating of `rating`.
+	 * Returns an empty set if none are found
 	 */
 	public Collection<Pastry> getPastriesForRating(int rating) {
 		/************************************************
- 	 	 *	WORK HERE, you must modify the return value
-		/************************************************/
-		return null;
+		 * WORK HERE, you must modify the return value /
+		 ************************************************/
+		// get set of keys
+		Set<Pastry> keys = ratingsHash.keySet();
+		// empty Set for collecting matches
+		Set<Pastry> matchingKeys = new HashSet<> ();
+		
+		// iterate
+		for (Pastry pastry : keys) {
+
+			if (ratingsHash.get(pastry) == rating) {
+				matchingKeys.add(pastry);
+			}
+		}
+		// return what's left - matching
+		return matchingKeys;
 	}
 
 }
