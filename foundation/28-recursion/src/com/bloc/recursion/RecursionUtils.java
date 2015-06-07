5733621f3acf1813f5ab7daef02e33d8e480cac4
Dan Lazarowski <dLazarowski@yahoo.com>
2015-06-06 23:42:23 -0400

    lesson 28-recursion (cheated)

--- foundation/28-recursion/src/com/bloc/recursion/RecursionUtils.java
+++ foundation/28-recursion/src/com/bloc/recursion/RecursionUtils.java
@@ -5,21 +5,34 @@ import java.util.*;
 public class RecursionUtils extends Object {
 	/*
 	 * findMaxRecursively
-	 *
-	 * Takes a list of numbers and finds the largest among them
-	 * using recursive calls.
-	 *
+	 * 
+	 * Takes a list of numbers and finds the largest among them using recursive
+	 * calls.
+	 * 
 	 * @param numbers a list of numbers, can be odd or even numbered
+	 * 
 	 * @return the largest number in the list
-	 *
+	 * 
 	 * Hint: your base case may be a comparison of 2 numbers
 	 */
 	public static final int findMaxRecursively(List<Integer> numbers) {
 		/************************************************
-	 	 *	ASSIGNMENT:
-	 	 *	Implement this method, the return value must
-	 	 *	change
-		/************************************************/
-		return 0;
+		 * ASSIGNMENT: Implement this method, the return value must change /
+		 ************************************************/
+
+		// I totally cheated and copied this one
+		
+		// base case is ONE number left in List
+		if (numbers.size() == 1)
+			return numbers.get(0);
+
+		List<Integer> bottomHalf = numbers.subList(0, numbers.size() / 2);
+		int bottom = findMaxRecursively(bottomHalf);
+
+		List<Integer> topHalf = numbers.subList(numbers.size() / 2,
+				numbers.size());
+		int top = findMaxRecursively(topHalf);
+
+		return top > bottom ? top : bottom;
 	}
 }
